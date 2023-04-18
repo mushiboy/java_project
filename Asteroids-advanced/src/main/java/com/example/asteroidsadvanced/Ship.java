77b1c22dff49604b57fa3d5a2be2115cbc570eb3
Mugesh <51223172+mushiboy@users.noreply.github.com>
2023-04-18 10:56:56 +0100

    Update Ship.java

--- Asteroids-advanced/src/main/java/com/example/asteroidsadvanced/Ship.java
+++ Asteroids-advanced/src/main/java/com/example/asteroidsadvanced/Ship.java
@@ -1,34 +1,66 @@
 package com.example.asteroidsadvanced;
 
+import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
+import javafx.scene.shape.Path;
 import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Shape;
+import javafx.util.Duration;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
-public class Ship extends Character {
+public class Ship extends Character{
+    private boolean isInvincible = false;
+
+    private boolean isHyperspaced = false;
 
     Timeline timeline = new Timeline();
-
-    // constructor for the Ship class, initializes a Polygon shape for the Ship
     public Ship(int x, int y) {
-        super(new Polygon(0, 0, 100, 25, 0, 50, 25, 25), x, y);
+        super(new Polygon(0,0,100,25,0,50,25,25), x, y);
     }
 
-    // Hyperspace method for the Ship class, randomly moves the Ship to a new location
+    public void setInvincible(boolean invincible) {
+        this.isInvincible = invincible;
+    }
+
+    public boolean isInvincible() {
+        return isInvincible;
+    }
+
+
     public void Hyperspace(){
 
-        // Unused code to move the ship by an angle and distance
 //        double angle = Math.toRadians(super.getCharacter().getRotate());
 //        double X = Math.cos(angle)*15;
 //        double Y = Math.sin(angle)*15;
 //
 //        super.getCharacter().setTranslateX(super.getCharacter().getTranslateX()+X);
 //        super.getCharacter().setTranslateY(super.getCharacter().getTranslateY()+Y);
-
-        // Generate random X and Y coordinates within 1000 units
+        boolean collision;
         Random rnd = new Random();
-        double X = rnd.nextInt(1000);
-        super.getCharacter().setTranslateX(rnd.nextInt(1000));
-        super.getCharacter().setTranslateY(rnd.nextInt(1000));
+        this.setHyperspaced(true);
+        super.getCharacter().setTranslateX(Math.random() * (1920 - 100  - this.getCharacter().getBoundsInParent().getWidth()));
+        super.getCharacter().setTranslateY(Math.random() * (1080 - 200 -this.getCharacter().getBoundsInParent().getHeight()));
+        super.setMovement(-super.getMovement().getX(), -super.getMovement().getY());
+    }
+
+
+    public boolean collide(Character other){
+        Shape collisionArea = Shape.intersect(super.getCharacter(), other.getCharacter());
+        return collisionArea.getBoundsInLocal().getWidth() != -1;
+    }
+
+    public boolean isHyperspaced() {
+        return isHyperspaced;
+    }
+
+    public void setHyperspaced(boolean hyperspaced) {
+        isHyperspaced = hyperspaced;
     }
 }
