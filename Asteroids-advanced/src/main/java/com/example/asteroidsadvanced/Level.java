ba433074a0925da9ae670d1a4cf1ae1e60afd360
CGebben <coltontgebben@gmail.com>
2023-04-20 16:30:38 +0100

    Revert "Changed aspect ratio"
    This reverts commit 045fc5bbdc6b80c54e2cae217390315853f2e5c8.

--- Asteroids-advanced/src/main/java/com/example/asteroidsadvanced/Level.java
+++ Asteroids-advanced/src/main/java/com/example/asteroidsadvanced/Level.java
@@ -19,7 +19,7 @@ public class Level {
     public static Level[] createLevels() {
         Level[] levels = new Level[14];
     
-        levels[0] = new Level(1, 2, 0, 0, 0);
+        levels[0] = new Level(1, 2, 0, 0, 1);
         levels[1] = new Level(2, 4, 0, 0, 0);
         levels[2] = new Level(3, 6, 0, 0, 0);
         levels[3] = new Level(4, 0, 2, 0, 0);
